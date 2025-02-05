3f1156741339d8e3bc25e3d68250ccdb3db37b2e
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2025-02-05 21:01:28 +0100

    Revert changes to unrelated test file

--- plugins/woocommerce/tests/php/src/Admin/Features/ShippingPartnerSuggestions/DefaultShippingPartnersTest.php
+++ plugins/woocommerce/tests/php/src/Admin/Features/ShippingPartnerSuggestions/DefaultShippingPartnersTest.php
@@ -129,4 +129,40 @@ class DefaultShippingPartnersTest extends WC_Unit_Test_Case {
 		self::rmdir( dirname( $tax_plugin_file_path ) );
 		self::delete_folders( dirname( $tax_plugin_file_path ) );
 	}
+
+	/**
+	 * Asserts no extensions are recommended if both WooCommerce Shipping and WooCommerce Tax are active.
+	 *
+	 * @return void
+	 */
+	public function test_no_extensions_are_recommended_if_both_woocommerce_shipping_and_woocommerce_tax_are_active() {
+		// Arrange.
+		// Make sure the plugin passes as active.
+		$shipping_plugin_file = 'woocommerce-shipping/woocommerce-shipping.php';
+		// To pass the validation, we need to the plugin file to exist.
+		$shipping_plugin_file_path = WP_PLUGIN_DIR . '/' . $shipping_plugin_file;
+		self::touch( $shipping_plugin_file_path );
+
+		// Make sure the plugin passes as active.
+		$tax_plugin_file = 'woocommerce-tax/woocommerce-tax.php';
+		// To pass the validation, we need to the plugin file to exist.
+		$tax_plugin_file_path = WP_PLUGIN_DIR . '/' . $tax_plugin_file;
+		self::touch( $tax_plugin_file_path );
+
+		update_option( 'active_plugins', array( $shipping_plugin_file, $tax_plugin_file ) );
+
+		// Act.
+		$specs   = DefaultShippingPartners::get_all();
+		$results = EvaluateSuggestion::evaluate_specs( $specs );
+
+		// Assert.
+		$this->assertCount( 0, $results['errors'] );
+		$this->assertCount( 0, $results['suggestions'] );
+
+		// Clean up.
+		self::rmdir( dirname( $shipping_plugin_file_path ) );
+		self::delete_folders( dirname( $shipping_plugin_file_path ) );
+		self::rmdir( dirname( $tax_plugin_file_path ) );
+		self::delete_folders( dirname( $tax_plugin_file_path ) );
+	}
 }
