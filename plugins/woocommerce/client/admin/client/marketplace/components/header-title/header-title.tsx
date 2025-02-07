a280f93d62fb77128bb01eeedd3e1a7cca2d8666
Raja Sekar Manimaran <raja.sekar.manimaran@automattic.com>
2025-02-07 18:03:19 +0530

    Split the header title into main title and sub title

--- plugins/woocommerce/client/admin/client/marketplace/components/header-title/header-title.tsx
+++ plugins/woocommerce/client/admin/client/marketplace/components/header-title/header-title.tsx
@@ -9,11 +9,16 @@ import { __ } from '@wordpress/i18n';
 
 export default function HeaderTitle() {
 	return (
-		<h1 className="woocommerce-marketplace__header-title">
-			{ __(
-				'Official WooCommerce Marketplace. Everything you need to grow your store.',
-				'woocommerce'
-			) }
-		</h1>
+		<div className="woocommerce-marketplace__header-title-container">
+			<h1 className="woocommerce-marketplace__header-title">
+				{ __( 'Official WooCommerce Marketplace', 'woocommerce' ) }
+			</h1>
+			<p className="woocommerce-marketplace__header-subtitle">
+				{ __(
+					'Everything you need to grow your store.',
+					'woocommerce'
+				) }
+			</p>
+		</div>
 	);
 }
