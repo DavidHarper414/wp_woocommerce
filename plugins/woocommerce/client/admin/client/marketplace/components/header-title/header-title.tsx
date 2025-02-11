01d1170de1c67c42e7222a4c544b4cf401786859
Raja Sekar Manimaran <raja.sekar.manimaran@automattic.com>
2025-02-11 17:03:01 +0530

    Remove extra text on header title and revert the css changes for adding a subtitle

--- plugins/woocommerce/client/admin/client/marketplace/components/header-title/header-title.tsx
+++ plugins/woocommerce/client/admin/client/marketplace/components/header-title/header-title.tsx
@@ -9,16 +9,8 @@ import { __ } from '@wordpress/i18n';
 
 export default function HeaderTitle() {
 	return (
-		<div className="woocommerce-marketplace__header-title-container">
-			<h1 className="woocommerce-marketplace__header-title">
-				{ __( 'Official WooCommerce Marketplace', 'woocommerce' ) }
-			</h1>
-			<p className="woocommerce-marketplace__header-subtitle">
-				{ __(
-					'Everything you need to grow your store.',
-					'woocommerce'
-				) }
-			</p>
-		</div>
+		<h1 className="woocommerce-marketplace__header-title">
+			{ __( 'Official WooCommerce Marketplace', 'woocommerce' ) }
+		</h1>
 	);
 }
