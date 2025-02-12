7249d22261b6d945ed35e4beb8d2dadafac161f7
KokkieH <KokkieH@users.noreply.github.com>
2025-02-12 15:23:58 +0200

    Add Marketplace link to Tax task

--- plugins/woocommerce/client/admin/client/task-lists/fills/tax/components/partners.tsx
+++ plugins/woocommerce/client/admin/client/task-lists/fills/tax/components/partners.tsx
@@ -5,6 +5,11 @@ import { __ } from '@wordpress/i18n';
 import { Button, Card, CardBody, CardHeader } from '@wordpress/components';
 import { Children } from '@wordpress/element';
 import clsx from 'clsx';
+import interpolateComponents from '@automattic/interpolate-components';
+import { Link } from '@woocommerce/components';
+import { getAdminLink } from '@woocommerce/settings';
+import { Text } from '@woocommerce/experimental';
+import { recordEvent } from '@woocommerce/tracks';
 
 /**
  * Internal dependencies
@@ -26,41 +31,74 @@ export const Partners = ( {
 		) }`
 	);
 	return (
-		<Card className={ classes }>
-			<CardHeader>
-				{ __( 'Choose a tax partner', 'woocommerce' ) }
-			</CardHeader>
-			<CardBody>
-				<div className="woocommerce-tax-partners__partners">
-					{ children }
-				</div>
-				<ul className="woocommerce-tax-partners__other-actions">
-					<li>
-						<Button
-							isTertiary
-							disabled={ isPending }
-							isBusy={ isPending }
-							onClick={ () => {
-								onManual();
-							} }
-						>
-							{ __( 'Set up taxes manually', 'woocommerce' ) }
-						</Button>
-					</li>
-					<li>
-						<Button
-							isTertiary
-							disabled={ isPending }
-							isBusy={ isPending }
-							onClick={ () => {
-								onDisable();
-							} }
-						>
-							{ __( 'I don’t charge sales tax', 'woocommerce' ) }
-						</Button>
-					</li>
-				</ul>
-			</CardBody>
-		</Card>
+		<>
+			<Card className={ classes }>
+				<CardHeader>
+					{ __( 'Choose a tax partner', 'woocommerce' ) }
+				</CardHeader>
+				<CardBody>
+					<div className="woocommerce-tax-partners__partners">
+						{ children }
+					</div>
+					<ul className="woocommerce-tax-partners__other-actions">
+						<li>
+							<Button
+								isTertiary
+								disabled={ isPending }
+								isBusy={ isPending }
+								onClick={ () => {
+									onManual();
+								} }
+							>
+								{ __( 'Set up taxes manually', 'woocommerce' ) }
+							</Button>
+						</li>
+						<li>
+							<Button
+								isTertiary
+								disabled={ isPending }
+								isBusy={ isPending }
+								onClick={ () => {
+									onDisable();
+								} }
+							>
+								{ __(
+									'I don’t charge sales tax',
+									'woocommerce'
+								) }
+							</Button>
+						</li>
+					</ul>
+				</CardBody>
+			</Card>
+			<Text
+				as="div"
+				className="woocommerce-task-dashboard__container woocommerce-task-marketplace-link"
+			>
+				{ interpolateComponents( {
+					mixedString: __(
+						'Visit the {{sbLink}}Official WooCommerce Marketplace{{/sbLink}} to enhance your products with additional marketing solutions.',
+						'woocommerce'
+					),
+					components: {
+						sbLink: (
+							<Link
+								onClick={ () => {
+									recordEvent(
+										'tasklist_marketing_visit_marketplace_click'
+									);
+									window.location.href = getAdminLink(
+										'admin.php?page=wc-admin&tab=extensions&path=/extensions&category=operations'
+									);
+									return false;
+								} }
+								href=""
+								type="wc-admin"
+							/>
+						),
+					},
+				} ) }
+			</Text>
+		</>
 	);
 };
