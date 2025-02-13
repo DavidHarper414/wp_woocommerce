cefa19fa87ee4fe4e391c9f9fd590bac0069eaec
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-02-13 15:56:08 +0800

    Update plugins store constant usage across components
    Replace PLUGINS_STORE_NAME with pluginsStore in multiple files, ensuring consistent import and usage of the plugins store across various components and task list files. This change follows the recent refactoring of the plugins store and maintains a uniform approach to store imports.

--- packages/js/components/src/plugins/index.tsx
+++ packages/js/components/src/plugins/index.tsx
@@ -11,7 +11,7 @@ import {
 } from '@wordpress/element';
 import { SyntheticEvent, useCallback } from 'react';
 import { useDispatch, useSelect } from '@wordpress/data';
-import { PLUGINS_STORE_NAME } from '@woocommerce/data';
+import { pluginsStore } from '@woocommerce/data';
 import type {
 	InstallPluginsResponse,
 	PluginSelectors,
@@ -59,14 +59,10 @@ export const Plugins = ( {
 	const [ hasErrors, setHasErrors ] = useState( false );
 	// Tracks action so that multiple instances of this button don't all light up when one is clicked
 	const [ hasBeenClicked, setHasBeenClicked ] = useState( false );
-	const { installAndActivatePlugins }: PluginActions =
-		useDispatch( PLUGINS_STORE_NAME );
+	const { installAndActivatePlugins } = useDispatch( pluginsStore );
 	const { isRequesting } = useSelect( ( select ) => {
-		const {
-			getActivePlugins,
-			getInstalledPlugins,
-			isPluginsRequesting,
-		}: PluginSelectors = select( PLUGINS_STORE_NAME );
+		const { getActivePlugins, getInstalledPlugins, isPluginsRequesting } =
+			select( pluginsStore );
 
 		return {
 			isRequesting:
--- plugins/woocommerce/client/admin/client/shipping/woocommerce-services-item.tsx
+++ plugins/woocommerce/client/admin/client/shipping/woocommerce-services-item.tsx
@@ -5,7 +5,7 @@ import { __ } from '@wordpress/i18n';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { Button, ExternalLink } from '@wordpress/components';
 import { Pill } from '@woocommerce/components';
-import { PLUGINS_STORE_NAME } from '@woocommerce/data';
+import { pluginsStore } from '@woocommerce/data';
 import { getAdminLink } from '@woocommerce/settings';
 
 /**
@@ -23,7 +23,7 @@ const WooCommerceServicesItem: React.FC< {
 	const isSiteConnectedToJetpack = useSelect(
 		( select ) =>
 			// @ts-expect-error Todo: awaiting more global fix, demo: https://github.com/woocommerce/woocommerce/pull/54146
-			select( PLUGINS_STORE_NAME ).isJetpackConnected(),
+			select( pluginsStore ).isJetpackConnected(),
 		[]
 	);
 
