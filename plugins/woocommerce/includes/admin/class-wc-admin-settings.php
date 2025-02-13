2814d811172928ee041e8e979bcbb6b4beece0bf
oaratovskyi <oleksandr.aratovskyi@automattic.com>
2025-02-13 20:53:25 +0200

    Attempt to fix failing tests

--- plugins/woocommerce/includes/admin/class-wc-admin-settings.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-settings.php
@@ -55,7 +55,8 @@ if ( ! class_exists( 'WC_Admin_Settings', false ) ) :
 				$settings[] = include __DIR__ . '/settings/class-wc-settings-products.php';
 				$settings[] = include __DIR__ . '/settings/class-wc-settings-tax.php';
 				$settings[] = include __DIR__ . '/settings/class-wc-settings-shipping.php';
-				if ( Features::is_enabled( 'reactify-classic-payments-settings' ) && Experimental_Abtest::in_treatment( 'woocommerce_payment_settings_2025_v1' ) ) {
+				// Special check for allowed tracking to prevent request failure.
+				if ( Features::is_enabled( 'reactify-classic-payments-settings' ) && 'yes' === get_option( 'woocommerce_allow_tracking', 'no' ) && Experimental_Abtest::in_treatment( 'woocommerce_payment_settings_2025_v1' ) ) {
 					$settings[] = include __DIR__ . '/settings/class-wc-settings-payment-gateways-react.php';
 				} else {
 					$settings[] = include __DIR__ . '/settings/class-wc-settings-payment-gateways.php';
