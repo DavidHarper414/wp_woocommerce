88610e6aaa0b6d01182afab9075ddb63af3135ad
Ahmed <ahmed.el.azzabi@automattic.com>
2025-02-14 09:01:11 +0100

    Update store key

--- plugins/woocommerce/client/admin/client/settings-payments/woopay-eligibility-store/constants.ts
+++ plugins/woocommerce/client/admin/client/settings-payments/woopay-eligibility-store/constants.ts
@@ -1,2 +1,2 @@
-export const STORE_KEY = 'wc/admin/settings/woopay-eligibility';
+export const STORE_KEY = 'wc/admin/payment-settings/woopay-eligibility';
 export const WC_ADMIN_NAMESPACE = '/wc-admin';
