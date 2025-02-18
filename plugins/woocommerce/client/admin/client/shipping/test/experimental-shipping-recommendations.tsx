0a27c63ff1085cfad1a04e6be48a27d550591d17
KokkieH <KokkieH@users.noreply.github.com>
2025-02-18 15:43:41 +0200

    Fix linting errors in tests

--- plugins/woocommerce/client/admin/client/shipping/test/experimental-shipping-recommendations.tsx
+++ plugins/woocommerce/client/admin/client/shipping/test/experimental-shipping-recommendations.tsx
@@ -149,12 +149,10 @@ describe( 'ShippingRecommendations', () => {
 	} );
 
 	it( 'should navigate to the marketplace when clicking the Official WooCommerce Marketplace link', async () => {
-		let oldLocation: Location;
 		const mockLocation = {
 			href: 'test',
 		} as Location;
 
-		oldLocation = global.window.location;
 		mockLocation.href = 'test';
 		Object.defineProperty( global.window, 'location', {
 			value: mockLocation,
