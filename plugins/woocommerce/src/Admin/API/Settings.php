30408928f79ddac61a62a0d79b7e9c2c32c1c5d3
paul sealock <psealock@gmail.com>
2025-02-19 12:37:25 +1300

    add tab and section query args to schema

--- plugins/woocommerce/src/Admin/API/Settings.php
+++ plugins/woocommerce/src/Admin/API/Settings.php
@@ -140,6 +140,18 @@ class Settings extends \WC_REST_Data_Controller {
 					'context'     => array( 'view' ),
 					'readonly'    => true,
 				),
+				'tab' => array(
+					'type'        => 'string',
+					'description' => __( 'Settings tab.', 'woocommerce' ),
+					'context'     => array( 'view', 'edit' ),
+					'default'     => 'general',
+				),
+				'section' => array(
+					'type'        => 'string', 
+					'description' => __( 'Settings section.', 'woocommerce' ),
+					'context'     => array( 'view', 'edit' ),
+					'default'     => '',
+				),
 			),
 		);
 
