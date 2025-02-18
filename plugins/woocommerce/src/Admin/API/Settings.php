b3b000b748fcc8cc030c12d3755f6e6968ad6c7a
paul sealock <psealock@gmail.com>
2025-02-19 11:21:08 +1300

    lint-fix

--- plugins/woocommerce/src/Admin/API/Settings.php
+++ plugins/woocommerce/src/Admin/API/Settings.php
@@ -44,7 +44,7 @@ class Settings extends \WC_REST_Data_Controller {
 					'methods'             => \WP_REST_Server::EDITABLE,
 					'callback'            => array( $this, 'save_settings' ),
 					'permission_callback' => array( $this, 'get_items_permissions_check' ),
-					'args'               => array(
+					'args'                => array(
 						'schema' => array( $this, 'save_items_schema' ),
 					),
 				),
@@ -69,35 +69,40 @@ class Settings extends \WC_REST_Data_Controller {
 	 * @return WP_Error|WP_REST_Response
 	 */
 	public function save_settings( $request ) {
-        global $current_section, $current_tab;
+		global $current_section, $current_tab;
 
-        $params = $request->get_params();
+		$params = $request->get_params();
 
-        try {
-            // Get current tab/section and set global variables.
-            $current_tab     = empty( $params['tab'] ) ? 'general' : sanitize_title( wp_unslash( $params['tab'] ) ); // WPCS: input var okay, CSRF ok.
-            $current_section = empty( $params['section'] ) ? '' : sanitize_title( wp_unslash( $params['section'] ) ); // WPCS: input var okay, CSRF ok.
+		try {
+			// Get current tab/section and set global variables.
+			$current_tab     = empty( $params['tab'] ) ? 'general' : sanitize_title( wp_unslash( $params['tab'] ) ); // WPCS: input var okay, CSRF ok.
+			$current_section = empty( $params['section'] ) ? '' : sanitize_title( wp_unslash( $params['section'] ) ); // WPCS: input var okay, CSRF ok.
 
-            $filter_name = '' === $current_section ? 
-            "woocommerce_save_settings_{$current_tab}" :
-            "woocommerce_save_settings_{$current_tab}_{$current_section}";
+			$filter_name = '' === $current_section ?
+			"woocommerce_save_settings_{$current_tab}" :
+			"woocommerce_save_settings_{$current_tab}_{$current_section}";
 
-            // Save settings if data has been posted
-            if ( apply_filters( $filter_name, ! empty( $_POST['save'] ) ) ) {
-                WC_Admin_Settings::save();
-            }
+			// Save settings if data has been posted
+			if ( apply_filters( $filter_name, ! empty( $_POST['save'] ) ) ) {
+				WC_Admin_Settings::save();
+			}
 
-            $setting_pages = \WC_Admin_Settings::get_settings_pages();
-            $settings = Init::get_page_data( array(), $setting_pages );
+			$setting_pages = \WC_Admin_Settings::get_settings_pages();
+			$settings      = Init::get_page_data( array(), $setting_pages );
 
-            return new \WP_REST_Response( array( 'status' => 'success', 'data' => $settings ) );
-        } catch ( \Exception $e ) {
-            return new \WP_Error(
-                'woocommerce_settings_save_error',
-                sprintf( __( 'Failed to save settings: %s', 'woocommerce' ), $e->getMessage() ),
-                array( 'status' => 500 )
-            );
-        }
+			return new \WP_REST_Response(
+				array(
+					'status' => 'success',
+					'data'   => $settings,
+				)
+			);
+		} catch ( \Exception $e ) {
+			return new \WP_Error(
+				'woocommerce_settings_save_error',
+				sprintf( __( 'Failed to save settings: %s', 'woocommerce' ), $e->getMessage() ),
+				array( 'status' => 500 )
+			);
+		}
 	}
 
 	public function save_items_schema() {
