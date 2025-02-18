a7deeda25f88135633ca89cc10d2c3f06e61012e
Vladimir Reznichenko <kalessil@gmail.com>
2025-02-18 08:00:17 +0100

    CI: cleanup.

--- tools/monorepo/check-changelogger-use.php
+++ tools/monorepo/check-changelogger-use.php
@@ -249,7 +249,7 @@ if ( ! $exit && getenv( 'CI' ) ) {
 		},
 		array_keys( $touched_projects )
 	);
-	echo sprintf( "Passed validation: %s", implode( ',', $passed_packages ) ), PHP_EOL;
+	echo sprintf( "Passed validation: %s", implode( ', ', $passed_packages ) ), PHP_EOL;
 }
 
 exit( $exit );
