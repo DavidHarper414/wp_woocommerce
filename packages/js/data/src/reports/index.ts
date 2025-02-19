205d7676993d19e10063e2cc14e146dcb19e2128
Luigi Teschio <gigitux@gmail.com>
2025-02-19 15:48:41 +0100

    remove not necessary comment

--- packages/js/data/src/reports/index.ts
+++ packages/js/data/src/reports/index.ts
@@ -28,7 +28,6 @@ import { PromiseifySelectors } from '../types/promiseify-selectors';
 export * from './types';
 export type { State };
 
-// @ts-expect-error migrate store to createReduxStore function https://github.com/woocommerce/woocommerce/issues/55381
 registerStore( STORE_NAME, {
 	reducer: reducer as Reducer< State, AnyAction >,
 	actions,
