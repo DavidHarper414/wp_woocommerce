1fe04cf9a64a62e67c290b2e9cd1b1f754773e93
Vladimir Reznichenko <kalessil@gmail.com>
2025-02-19 08:37:52 +0100

    CI: in CI start only test containers.

--- plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
+++ plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
@@ -3,6 +3,7 @@
 if [ ! -z ${CI+y} ]; then
     wp-env run tests-cli pwd
     wp-env run tests-cli stat ~/.cache/composer
+    wp-env run tests-cli stat wp-content/plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
     # wp-env run tests-cli "pwd && stat ~/.cache/composer && stat /var/www/html/wp-content/plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh"
 fi
 
