0d0cadad125221c2e315dbd5b88c0c5713ab986c
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-02-20 18:09:07 +0100

    Migrate Product Collection from WC to WP iAPI (#55330)
    * Fix iAPI navigation in Product Collection
    
    * Extract dataset keys to a variavle
    
    * Remove the loading bar animation and rely on iAPI one
    
    * Fix lint
    
    * Refactor the dataset access
    
    * Fix lint
    
    * Partially fix E2E test
    
    * Move variables to if statements where they are used and fix types
    
    * Remove code with accessibility messgaes as it's handled natively by iAPI
    
    * Update comment from navigation-id to router-region
    
    * Replace scroll calculations and functionality with a simple focus()
    
    * Rely on core/router clientNavigationDisabled instead of custom attribute
    
    * Refactor link validation and focus handling in product collection navigation
    
    * Avoid using random values for data-wp-key
    
    * Consolidate  an unnecessary function and fix PHP lint issue
    
    * Don't parse/create unnecessary context
    
    * Minor lint issue
    
    * Don't overwrite context, as it is already been declared
    
    * Consolidate all the directives in a single function
    
    With so many functions, is difficult to know which directives are
    being added and where. Also, it's unnecessary (and wasteful) to
    initialize more than one HTML Tag Processor to add these directives.
    
    ---------
    
    Co-authored-by: Tom Cafferkey <tjcafferkey@gmail.com>
    Co-authored-by: Luis Herranz <luisherranz@gmail.com>

--- plugins/woocommerce-blocks/assets/js/blocks/product-collection/frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-collection/frontend.tsx
@@ -16,108 +16,44 @@ import './style.scss';
 export type ProductCollectionStoreContext = {
 	// Available on the <li/> product element and deeper
 	productId?: number;
-	isPrefetchNextOrPreviousLink: boolean;
-	animation: 'start' | 'finish';
-	accessibilityMessage: string;
-	accessibilityLoadingMessage: string;
-	accessibilityLoadedMessage: string;
+	isPrefetchNextOrPreviousLink: string;
 	collection: CoreCollectionNames;
 };
 
-const isValidLink = ( ref: HTMLAnchorElement ) =>
-	ref &&
-	ref instanceof window.HTMLAnchorElement &&
-	ref.href &&
-	( ! ref.target || ref.target === '_self' ) &&
-	ref.origin === window.location.origin;
+function isValidLink( ref: HTMLElement | null ): ref is HTMLAnchorElement {
+	return (
+		ref !== null &&
+		ref instanceof window.HTMLAnchorElement &&
+		!! ref.href &&
+		( ! ref.target || ref.target === '_self' ) &&
+		ref.origin === window.location.origin
+	);
+}
 
-const isValidEvent = ( event: MouseEvent ) =>
-	event.button === 0 && // Left clicks only.
-	! event.metaKey && // Open in new tab (Mac).
-	! event.ctrlKey && // Open in new tab (Windows).
-	! event.altKey && // Download.
-	! event.shiftKey &&
-	! event.defaultPrevented;
-
-const forcePageReload = ( href: string ) => {
-	window.location.assign( href );
-	// It's function called in generator expecting asyncFunc return.
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	return new Promise( () => {} );
-};
-
-/**
- * Ensures the visibility of the first product in the collection.
- * Scrolls the page to the first product if it's not in the viewport.
- *
- * @param {string} wcNavigationId Unique ID for each Product Collection block on page/post.
- */
-function scrollToFirstProductIfNotVisible( wcNavigationId?: string ) {
-	if ( ! wcNavigationId ) {
-		return;
-	}
-
-	const productSelector = `[data-wp-router-region=${ wcNavigationId }] .wc-block-product-template .wc-block-product`;
-	const product = document.querySelector( productSelector );
-	if ( product ) {
-		const rect = product.getBoundingClientRect();
-		const isVisible =
-			rect.top >= 0 &&
-			rect.left >= 0 &&
-			rect.bottom <=
-				( window.innerHeight ||
-					document.documentElement.clientHeight ) &&
-			rect.right <=
-				( window.innerWidth || document.documentElement.clientWidth );
-
-		// If the product is not visible, scroll to it.
-		if ( ! isVisible ) {
-			product.scrollIntoView( {
-				behavior: 'smooth',
-				block: 'start',
-			} );
-		}
-	}
+function isValidEvent( event: MouseEvent ): boolean {
+	return (
+		event.button === 0 && // Left clicks only.
+		! event.metaKey && // Open in new tab (Mac).
+		! event.ctrlKey && // Open in new tab (Windows).
+		! event.altKey && // Download.
+		! event.shiftKey &&
+		! event.defaultPrevented
+	);
 }
 
 const productCollectionStore = {
-	state: {
-		get startAnimation() {
-			return (
-				getContext< ProductCollectionStoreContext >().animation ===
-				'start'
-			);
-		},
-		get finishAnimation() {
-			return (
-				getContext< ProductCollectionStoreContext >().animation ===
-				'finish'
-			);
-		},
-	},
 	actions: {
 		*navigate( event: MouseEvent ) {
-			const ctx = getContext< ProductCollectionStoreContext >();
 			const { ref } = getElement();
-			const wcNavigationId = (
-				ref?.closest( '[data-wp-router-region]' ) as HTMLDivElement
-			 )?.dataset?.wcNavigationId;
-			const isDisabled = (
-				ref?.closest( '[data-wp-router-region]' ) as HTMLDivElement
-			 )?.dataset.wcNavigationDisabled;
-
-			if ( isDisabled ) {
-				yield forcePageReload( ref.href );
-			}
 
 			if ( isValidLink( ref ) && isValidEvent( event ) ) {
 				event.preventDefault();
 
-				// Don't start animation if it doesn't take long to navigate.
-				const timeout = setTimeout( () => {
-					ctx.accessibilityMessage = ctx.accessibilityLoadingMessage;
-					ctx.animation = 'start';
-				}, 400 );
+				const ctx = getContext< ProductCollectionStoreContext >();
+
+				const routerRegionId = ref
+					.closest( '[data-wp-router-region]' )
+					?.getAttribute( 'data-wp-router-region' );
 
 				const { actions } = yield import(
 					'@wordpress/interactivity-router'
@@ -125,23 +61,14 @@ const productCollectionStore = {
 
 				yield actions.navigate( ref.href );
 
-				// Clear the timeout if the navigation is fast.
-				clearTimeout( timeout );
+				ctx.isPrefetchNextOrPreviousLink = ref.href;
 
-				// Announce that the page has been loaded. If the message is the
-				// same, we use a no-break space similar to the @wordpress/a11y
-				// package: https://github.com/WordPress/gutenberg/blob/c395242b8e6ee20f8b06c199e4fc2920d7018af1/packages/a11y/src/filter-message.js#L20-L26
-				ctx.accessibilityMessage =
-					ctx.accessibilityLoadedMessage +
-					( ctx.accessibilityMessage ===
-					ctx.accessibilityLoadedMessage
-						? '\u00A0'
-						: '' );
-
-				ctx.animation = 'finish';
-				ctx.isPrefetchNextOrPreviousLink = !! ref.href;
-
-				scrollToFirstProductIfNotVisible( wcNavigationId );
+				// Moves focus to the product link.
+				const product: HTMLAnchorElement | null =
+					document.querySelector(
+						`[data-wp-router-region=${ routerRegionId }] .wc-block-product-template .wc-block-product a`
+					);
+				product?.focus();
 
 				triggerProductListRenderedEvent( {
 					collection: ctx.collection,
@@ -154,14 +81,6 @@ const productCollectionStore = {
 		 */
 		*prefetchOnHover() {
 			const { ref } = getElement();
-
-			const isDisabled = (
-				ref?.closest( '[data-wp-router-region]' ) as HTMLDivElement
-			 )?.dataset.wcNavigationDisabled;
-
-			if ( isDisabled ) {
-				return;
-			}
 
 			if ( isValidLink( ref ) ) {
 				const { actions } = yield import(
@@ -186,17 +105,9 @@ const productCollectionStore = {
 		 */
 		*prefetch() {
 			const { ref } = getElement();
-			const isDisabled = (
-				ref?.closest( '[data-wp-router-region]' ) as HTMLDivElement
-			 )?.dataset.wcNavigationDisabled;
-
-			if ( isDisabled ) {
-				return;
-			}
-
 			const context = getContext< ProductCollectionStoreContext >();
 
-			if ( context?.isPrefetchNextOrPreviousLink && isValidLink( ref ) ) {
+			if ( isValidLink( ref ) && context.isPrefetchNextOrPreviousLink ) {
 				const { actions } = yield import(
 					'@wordpress/interactivity-router'
 				);
--- plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection/Renderer.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection/Renderer.php
@@ -17,10 +17,10 @@ class Renderer {
 	 *
 	 * @var array
 	 */
-	private $render_state = [
+	private $render_state = array(
 		'has_results'          => false,
 		'has_no_results_block' => false,
-	];
+	);
 
 	/**
 	 * The Block with its attributes before it gets rendered
@@ -114,7 +114,7 @@ class Renderer {
 	 * Enhances the Product Collection block with client-side pagination.
 	 *
 	 * This function identifies Product Collection blocks and adds necessary data attributes
-	 * to enable client-side navigation and animation effects. It also enqueues the Interactivity API runtime.
+	 * to enable client-side navigation. It also enqueues the Interactivity API runtime.
 	 *
 	 * @param string $block_content The HTML content of the block.
 	 * @param array  $block         Block details, including its attributes.
@@ -126,43 +126,37 @@ class Renderer {
 
 		if ( $is_product_collection_block ) {
 			wp_enqueue_script_module( 'woocommerce/product-collection' );
+
+			$collection                     = $block['attrs']['collection'] ?? '';
+			$is_enhanced_pagination_enabled = ! ( $block['attrs']['forcePageReload'] ?? false );
+
 			$p = new \WP_HTML_Tag_Processor( $block_content );
-			if ( $this->is_next_tag_product_collection( $p ) ) {
-				$this->set_product_collection_namespace( $p );
+			if ( $p->next_tag( array( 'class_name' => 'wp-block-woocommerce-product-collection' ) ) ) {
+				$p->set_attribute( 'data-wp-interactive', 'woocommerce/product-collection' );
+				$p->set_attribute( 'data-wp-init', 'callbacks.onRender' );
+				$p->set_attribute(
+					'data-wp-context',
+					$collection ? wp_json_encode(
+						array( 'collection' => $collection ),
+						JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
+					) : '{}'
+				);
+
+				if ( $is_enhanced_pagination_enabled && isset( $this->parsed_block ) ) {
+					$p->set_attribute(
+						'data-wp-router-region',
+						'wc-product-collection-' . $this->parsed_block['attrs']['queryId']
+					);
+				}
 			}
+
 			// Check if dimensions need to be set and handle accordingly.
 			$this->handle_block_dimensions( $p, $block );
+
 			$block_content = $p->get_updated_html();
-
-			$collection    = $block['attrs']['collection'] ?? '';
-			$block_content = $this->add_rendering_callback( $block_content, $collection );
-
-			$is_enhanced_pagination_enabled = ! ( $block['attrs']['forcePageReload'] ?? false );
-			if ( $is_enhanced_pagination_enabled ) {
-				$block_content = $this->enable_client_side_navigation( $block_content );
-			}
 		}
+
 		return $block_content;
-	}
-
-	/**
-	 * Check if next tag is a PC block.
-	 *
-	 * @param WP_HTML_Tag_processor $p Initial tag processor.
-	 *
-	 * @return bool Answer if PC block is available.
-	 */
-	private function is_next_tag_product_collection( $p ) {
-		return $p->next_tag( array( 'class_name' => 'wp-block-woocommerce-product-collection' ) );
-	}
-
-	/**
-	 * Set PC block namespace for Interactivity API.
-	 *
-	 * @param WP_HTML_Tag_processor $p Initial tag processor.
-	 */
-	private function set_product_collection_namespace( $p ) {
-		$p->set_attribute( 'data-wp-interactive', 'woocommerce/product-collection' );
 	}
 
 	/**
@@ -206,102 +200,6 @@ class Renderer {
 	}
 
 	/**
-	 * Attach the init directive to Product Collection block to call
-	 * the onRender callback.
-	 *
-	 * @param string $block_content The HTML content of the block.
-	 * @param string $collection Collection type.
-	 *
-	 * @return string Updated HTML content.
-	 */
-	private function add_rendering_callback( $block_content, $collection ) {
-		$p = new \WP_HTML_Tag_Processor( $block_content );
-
-		// Add `data-init to the product collection block so we trigger JS event on render.
-		if ( $this->is_next_tag_product_collection( $p ) ) {
-			$p->set_attribute(
-				'data-wp-init',
-				'callbacks.onRender'
-			);
-			$p->set_attribute(
-				'data-wp-context',
-				$collection ? wp_json_encode(
-					array( 'collection' => $collection ),
-					JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
-				) : '{}'
-			);
-		}
-
-		return $p->get_updated_html();
-	}
-
-	/**
-	 * Attach all the Interactivity API directives responsible
-	 * for client-side navigation.
-	 *
-	 * @param string $block_content The HTML content of the block.
-	 *
-	 * @return string Updated HTML content.
-	 */
-	private function enable_client_side_navigation( $block_content ) {
-		$p = new \WP_HTML_Tag_Processor( $block_content );
-
-		// Add `data-wp-navigation-id to the product collection block.
-		if ( $this->is_next_tag_product_collection( $p ) && isset( $this->parsed_block ) ) {
-			$p->set_attribute(
-				'data-wp-navigation-id',
-				'wc-product-collection-' . $this->parsed_block['attrs']['queryId']
-			);
-			$current_context = json_decode( $p->get_attribute( 'data-wp-context' ) ?? '{}', true );
-			$p->set_attribute(
-				'data-wp-context',
-				wp_json_encode(
-					array_merge(
-						$current_context,
-						array(
-							// The message to be announced by the screen reader when the page is loading or loaded.
-							'accessibilityLoadingMessage'  => __( 'Loading page, please wait.', 'woocommerce' ),
-							'accessibilityLoadedMessage'   => __( 'Page Loaded.', 'woocommerce' ),
-							// We don't prefetch the links if user haven't clicked on pagination links yet.
-							// This way we avoid prefetching when the page loads.
-							'isPrefetchNextOrPreviousLink' => false,
-						),
-					),
-					JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
-				)
-			);
-			$block_content = $p->get_updated_html();
-		}
-
-		/**
-		 * Add two div's:
-		 * 1. Pagination animation for visual users.
-		 * 2. Accessibility div for screen readers, to announce page load states.
-		 */
-		$last_tag_position                = strripos( $block_content, '</div>' );
-		$accessibility_and_animation_html = '
-				<div
-					data-wp-interactive="woocommerce/product-collection"
-					class="wc-block-product-collection__pagination-animation"
-					data-wp-class--start-animation="state.startAnimation"
-					data-wp-class--finish-animation="state.finishAnimation">
-				</div>
-				<div
-					data-wp-interactive="woocommerce/product-collection"
-					class="screen-reader-text"
-					aria-live="polite"
-					data-wp-text="context.accessibilityMessage">
-				</div>
-			';
-		return substr_replace(
-			$block_content,
-			$accessibility_and_animation_html,
-			$last_tag_position,
-			0
-		);
-	}
-
-	/**
 	 * Add interactive links to all anchors inside the Query Pagination block.
 	 * This enabled client-side navigation for the product collection block.
 	 *
@@ -319,62 +217,30 @@ class Renderer {
 		// Only proceed if the block is a product collection block,
 		// enhanced pagination is enabled and query IDs match.
 		if ( $is_product_collection_block && $is_enhanced_pagination_enabled && $query_id === $parsed_query_id ) {
-			$block_content = $this->process_pagination_links( $block_content );
+			$p = new \WP_HTML_Tag_Processor( $block_content );
+			$p->next_tag( array( 'class_name' => 'wp-block-query-pagination' ) );
+
+			while ( $p->next_tag( 'A' ) ) {
+				if ( $p->has_class( 'wp-block-query-pagination-next' ) || $p->has_class( 'wp-block-query-pagination-previous' ) ) {
+					$p->set_attribute( 'data-wp-on--click', 'woocommerce/product-collection::actions.navigate' );
+					$p->set_attribute(
+						'data-wp-key',
+						$p->has_class( 'wp-block-query-pagination-next' )
+							? 'product-collection-pagination--next'
+							: 'product-collection-pagination--previous'
+					);
+					$p->set_attribute( 'data-wp-watch', 'woocommerce/product-collection::callbacks.prefetch' );
+					$p->set_attribute( 'data-wp-on--mouseenter', 'woocommerce/product-collection::actions.prefetchOnHover' );
+				} elseif ( $p->has_class( 'page-numbers' ) ) {
+					$p->set_attribute( 'data-wp-on--click', 'woocommerce/product-collection::actions.navigate' );
+					$p->set_attribute( 'data-wp-key', 'product-collection-pagination-numbers--' . $p->get_attribute( 'aria-label' ) );
+				}
+			}
+
+			return $p->get_updated_html();
 		}
 
 		return $block_content;
-	}
-
-	/**
-	 * Process pagination links within the block content.
-	 *
-	 * @param string $block_content The block content.
-	 * @return string The updated block content.
-	 */
-	private function process_pagination_links( $block_content ) {
-		if ( ! $block_content ) {
-			return $block_content;
-		}
-
-		$p = new \WP_HTML_Tag_Processor( $block_content );
-		$p->next_tag( array( 'class_name' => 'wp-block-query-pagination' ) );
-
-		// This will help us to find the start of the block content using the `seek` method.
-		$p->set_bookmark( 'start' );
-
-		$this->update_pagination_anchors( $p, 'page-numbers', 'product-collection-pagination-numbers' );
-		$this->update_pagination_anchors( $p, 'wp-block-query-pagination-next', 'product-collection-pagination--next' );
-		$this->update_pagination_anchors( $p, 'wp-block-query-pagination-previous', 'product-collection-pagination--previous' );
-
-		return $p->get_updated_html();
-	}
-
-	/**
-	 * Sets up data attributes required for interactivity and client-side navigation.
-	 *
-	 * @param \WP_HTML_Tag_Processor $processor The HTML tag processor.
-	 * @param string                 $class_name The class name of the anchor tags.
-	 * @param string                 $key_prefix The prefix for the data-wp-key attribute.
-	 */
-	private function update_pagination_anchors( $processor, $class_name, $key_prefix ) {
-		// Start from the beginning of the block content.
-		$processor->seek( 'start' );
-
-		while ( $processor->next_tag(
-			array(
-				'tag_name'   => 'a',
-				'class_name' => $class_name,
-			)
-		) ) {
-			$this->set_product_collection_namespace( $processor );
-			$processor->set_attribute( 'data-wp-on--click', 'actions.navigate' );
-			$processor->set_attribute( 'data-wp-key', $key_prefix . '--' . esc_attr( wp_rand() ) );
-
-			if ( in_array( $class_name, array( 'wp-block-query-pagination-next', 'wp-block-query-pagination-previous' ), true ) ) {
-				$processor->set_attribute( 'data-wp-watch', 'callbacks.prefetch' );
-				$processor->set_attribute( 'data-wp-on--mouseenter', 'actions.prefetchOnHover' );
-			}
-		}
 	}
 
 	/**
