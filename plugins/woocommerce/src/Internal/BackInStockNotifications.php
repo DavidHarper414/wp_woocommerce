96acab6fbf622635f9b9661cf0386d8aca53e8b6
Peter Fabian <peter.fabian.github@gmail.com>
2025-02-20 11:53:10 +0100

    Disable signups for stores that didn't have BIS active.

--- plugins/woocommerce/src/Internal/BackInStockNotifications.php
+++ plugins/woocommerce/src/Internal/BackInStockNotifications.php
@@ -17,9 +17,26 @@ defined( 'ABSPATH' ) || exit;
  */
 class BackInStockNotifications {
 
+	/**
+	 * Database utility instance.
+	 * 
+	 * @var DatabaseUtil
+	 */
 	private static $db_utils;
 
+	/**
+	 * Whether this is an activation request for BIS.
+	 * 
+	 * @var bool
+	 */
 	private static $is_activation_request = false;
+
+	/**
+	 * Whether the standalone BIS plugin is active.
+	 * 
+	 * @var bool
+	 */
+	private static $bis_plugin_is_active = false;
 
 	/**
 	 * Class initialization
@@ -29,7 +46,7 @@ class BackInStockNotifications {
 	final public static function init() {
 
 		// If this is an activation request for BIS, included code can't be loaded, as it will end up with a fatal error.
-		if ( ! self::is_enabled() || self::$is_activation_request ) { 
+		if ( ! self::is_enabled() || self::$is_activation_request || self::$bis_plugin_is_active ) { 
 			return;
 		}
 
@@ -41,12 +58,40 @@ class BackInStockNotifications {
 		add_action( 'add_option_wc_feature_woocommerce_back_in_stock_notifications_enabled', array( __CLASS__, 'handle_add_option' ), 10, 2 );
 		add_action( 'delete_option_wc_feature_woocommerce_back_in_stock_notifications_enabled', array( __CLASS__, 'handle_delete_option' ), 10, 1 );
 
+		// Deactivate signups for BIS to prevent changing the single product screen.
+		add_action( 'woocommerce_updated', array( __CLASS__, 'maybe_deactivate_signups' ) );
+
 		// Include BIS files.
 		include_once WC_ABSPATH . '/includes/bis/class-wc-bis-notifications.php';
 
 		$wc_bis = wc_get_container()->get( LegacyProxy::class )->call_function( 'WC_BIS' );
 		$wc_bis->initialize_plugin();
 
+	}
+
+	/**
+	 * Deactivate signups for BIS to prevent changing the single product screen.
+	 * 
+	 * This should be called from the WooCommerce update hook.
+	 * 
+	 * If standalone BIS plugin was active, it won't change the signups option.
+	 * If standalone BIS plugin was not active, it will deactivate signups to prevent 
+	 * changing the single product screen.
+	 * 
+	 * @return void
+	 */
+	public static function maybe_deactivate_signups() {
+		// Check if we've already done the initial setup.
+		if ( 'yes' === get_option( 'wc_bis_core_initialized' ) ) {
+			return;
+		}	
+	
+		if ( ! self::$bis_plugin_is_active ) {
+			update_option( 'wc_bis_allow_signups', 'no' );
+		}
+
+		// Mark as initialized so we don't run this again.
+		update_option( 'wc_bis_core_initialized', 'yes' );
 	}
 
 	/**
@@ -93,10 +138,10 @@ class BackInStockNotifications {
 			// BIS plugin is then deactivated during plugins_loaded@10 priority from \Automattic\WooCommerce\Packages::on_init().
 			remove_action( 'plugins_loaded', array( WC_BIS(), 'initialize_plugin' ), 9 );
 
-			// Strictly speaking, it's not an activation request, but when WC_BIS() is present before
-			// loading BIS from core, it will fatal during init(), so init() needs to be skipped.
-			// This should only be triggered once after a plugin update during the reuqest when BIS plugin is deactivated.
-			self::$is_activation_request = true;
+			// When WC_BIS() is present before loading BIS from core, it will fatal during init(), 
+			// so init() needs to be skipped. This should only be triggered once after 
+			// a plugin update during the request when BIS plugin is deactivated.
+			self::$bis_plugin_is_active = true;
 		}
 
 		// Set flag for activation request to prevent fatal errors when BIS is activated while WC+BIS is already active.
