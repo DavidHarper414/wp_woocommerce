7bc7f97920557854024fb653a95d3d5fe790c6cc
roykho <roykho77@gmail.com>
2025-02-20 07:05:57 -0800

    Remove get-allowed-blocks from frontend loading

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/utils/get-allowed-blocks.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/utils/get-allowed-blocks.ts
@@ -1,11 +1,16 @@
 /**
+ * External dependencies
+ */
+import { getBlockTypes } from '@wordpress/blocks';
+
+/**
  * Returns an array of allowed block names excluding the specified blocks.
  *
  * @param excludedBlocks Array of block names to exclude from the list.
  * @return Array of allowed block names.
  */
 export const getAllowedBlocks = ( excludedBlocks: string[] = [] ) => {
-	const allBlocks = window.wp.data.select( 'core/blocks' ).getBlockTypes();
+	const allBlocks = getBlockTypes();
 
 	return allBlocks
 		.map( ( block ) => block.name )
