48d7ad939b3250ab007bd3655d187294766b0e8e
roykho <roykho77@gmail.com>
2025-02-20 07:20:52 -0800

    Remove unused file

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/utils/index.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/utils/index.ts
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-export * from './get-client-id-by-block-name';
 export * from './get-inner-block-by-name';
 export * from './get-colors-from-block-supports';
 export * from './price-currency';
