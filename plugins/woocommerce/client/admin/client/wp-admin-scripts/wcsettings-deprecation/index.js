bcc0bdee485f7db057420c80683e17fb2fc226c1
moon <moon.kyong@automattic.com>
2025-02-22 13:14:37 -0800

    Fix env import

--- plugins/woocommerce/client/admin/client/wp-admin-scripts/wcsettings-deprecation/index.js
+++ plugins/woocommerce/client/admin/client/wp-admin-scripts/wcsettings-deprecation/index.js
@@ -2,16 +2,13 @@
  * Internal dependencies
  */
 import { createDeprecatedObjectProxy } from '../../utils';
-import {
-	deprecatedAdminProperties,
-	isDevelopmentEnvironment,
-} from '../../utils/admin-settings';
+import { deprecatedAdminProperties } from '../../utils/admin-settings';
 
 if (
 	window.wcSettings &&
 	deprecatedAdminProperties &&
 	Object.keys( deprecatedAdminProperties ).length > 0 &&
-	isDevelopmentEnvironment
+	process.env.NODE_ENV === 'development'
 ) {
 	wcSettings = createDeprecatedObjectProxy( wcSettings, {
 		admin: deprecatedAdminProperties,
