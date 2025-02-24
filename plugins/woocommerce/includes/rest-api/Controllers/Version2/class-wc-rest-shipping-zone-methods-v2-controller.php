f4bd86b458fa7e9ce6aa2cd1b570cea579c75240
Jorge A. Torres <jorge.torres@automattic.com>
2025-02-24 08:10:02 -0800

    Update class-wc-rest-shipping-zone-methods-v2-controller.php

--- plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-shipping-zone-methods-v2-controller.php
+++ plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-shipping-zone-methods-v2-controller.php
@@ -150,7 +150,7 @@ class WC_REST_Shipping_Zone_Methods_V2_Controller extends WC_REST_Shipping_Zones
 		$total    = count( $data );
 		$response = rest_ensure_response( $data );
 		$response->header( 'X-WP-Total', $total );
-		$response->header( 'X-WP-TotalPages', 1 );
+		$response->header( 'X-WP-TotalPages', $total ? 1 : 0 );
 		return $response;
 	}
 
