653a701521438541cde8656b662d1eba7fd140ab
Jorge A. Torres <jorge.torres@automattic.com>
2025-02-24 08:09:40 -0800

    Update class-wc-rest-shipping-zone-locations-v2-controller.php

--- plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-shipping-zone-locations-v2-controller.php
+++ plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-shipping-zone-locations-v2-controller.php
@@ -71,7 +71,7 @@ class WC_REST_Shipping_Zone_Locations_V2_Controller extends WC_REST_Shipping_Zon
 		$total    = count( $data );
 		$response = rest_ensure_response( $data );
 		$response->header( 'X-WP-Total', $total );
-		$response->header( 'X-WP-TotalPages', 1 );
+		$response->header( 'X-WP-TotalPages', $total ? 1 : 0 );
 		return $response;
 	}
 
