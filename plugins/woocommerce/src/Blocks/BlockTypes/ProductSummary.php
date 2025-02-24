2c494d9bb670879a136f52dbb06b134f73f8f7e9
Luigi Teschio <gigitux@gmail.com>
2025-02-24 16:55:19 +0100

    add comment

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductSummary.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductSummary.php
@@ -75,6 +75,7 @@ class ProductSummary extends AbstractBlock {
 		$short_description = $product->get_short_description();
 
 		if ( $short_description ) {
+			// Logic copied from https://github.com/woocommerce/woocommerce/blob/637dde283057ed6667ff81c73ed08774552f631d/plugins/woocommerce/includes/wc-core-functions.php#L53-L62.
 			$short_description = wp_kses_post( $short_description );
 			$short_description = $GLOBALS['wp_embed']->run_shortcode( $short_description );
 			$short_description = shortcode_unautop( $short_description );
@@ -85,6 +86,7 @@ class ProductSummary extends AbstractBlock {
 		$description = $product->get_description();
 
 		if ( $show_description_if_empty && $description ) {
+			// Logic copied from https://github.com/woocommerce/woocommerce/blob/637dde283057ed6667ff81c73ed08774552f631d/plugins/woocommerce/includes/wc-core-functions.php#L53-L62.
 			$description = wp_kses_post( $description );
 			$description = $GLOBALS['wp_embed']->run_shortcode( $description );
 			$description = shortcode_unautop( $description );
