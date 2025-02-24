be32e89da6126eff85297355450ff214a3c4a280
Roy Ho <roykho77@gmail.com>
2025-02-23 21:52:37 -0800

    [Experimental] Product filters: iapi migration (#55462)
    * Change to use data-wp directive namespace
    * Replace @woocommerce/interactivity with @wordpress/interactivity
    * Add product filters block entries to webpack
    * Enqueue module scripts
    * Override block script and return null

--- plugins/woocommerce-blocks/bin/webpack-config-interactivity-blocks-frontend.js
+++ plugins/woocommerce-blocks/bin/webpack-config-interactivity-blocks-frontend.js
@@ -19,6 +19,26 @@ const blockEntries = {
 		'./assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image/frontend.tsx',
 	'woocommerce/product-collection':
 		'./assets/js/blocks/product-collection/frontend.tsx',
+	'woocommerce/product-filters':
+		'./assets/js/blocks/product-filters/frontend.ts',
+	'woocommerce/product-filter-active':
+		'./assets/js/blocks/product-filters/inner-blocks/active-filters/frontend.ts',
+	'woocommerce/product-filter-attribute':
+		'./assets/js/blocks/product-filters/inner-blocks/attribute-filter/frontend.ts',
+	'woocommerce/product-filter-checkbox-list':
+		'./assets/js/blocks/product-filters/inner-blocks/checkbox-list/frontend.ts',
+	'woocommerce/product-filter-chips':
+		'./assets/js/blocks/product-filters/inner-blocks/chips/frontend.ts',
+	'woocommerce/product-filter-price':
+		'./assets/js/blocks/product-filters/inner-blocks/price-filter/frontend.ts',
+	'woocommerce/product-filter-price-slider':
+		'./assets/js/blocks/product-filters/inner-blocks/price-slider/frontend.ts',
+	'woocommerce/product-filter-rating':
+		'./assets/js/blocks/product-filters/inner-blocks/rating-filter/frontend.ts',
+	'woocommerce/product-filter-removable-chips':
+		'./assets/js/blocks/product-filters/inner-blocks/removable-chips/frontend.ts',
+	'woocommerce/product-filter-status':
+		'./assets/js/blocks/product-filters/inner-blocks/status-filter/frontend.ts',
 };
 
 module.exports = {
