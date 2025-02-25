47aaf66348d207889027ab237c457771d0f8e518
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-02-25 14:05:04 +0800

    Remove product editor import

--- plugins/woocommerce/client/admin/client/settings-email/settings-email-preview-send.tsx
+++ plugins/woocommerce/client/admin/client/settings-email/settings-email-preview-send.tsx
@@ -29,6 +29,12 @@ type WPError = {
 	};
 };
 
+const isValidEmail = ( email: string ) => {
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test( String( email ).toLowerCase() );
+};
+
 export const EmailPreviewSend: React.FC< EmailPreviewSendProps > = ( {
 	type,
 } ) => {
