3af3736f830478b01eba47caed19f92a503e6334
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-02-25 16:01:48 +0800

    Add embedded layout support for WooCommerce admin
    - Introduce new embed entry point in webpack config
    - Create new embed-specific components for header, layout, and entry point
    - Update WCAdminAssets to support embedded page script loading

---
+++ plugins/woocommerce/client/admin/client/embed.tsx
@@ -0,0 +1,17 @@
+/**
+ * Internal dependencies
+ */
+import './stylesheets/_embed.scss';
+import { renderCustomerEffortScoreTracks } from './shared';
+import { getAdminSetting } from '~/utils/admin-settings';
+import { renderEmbeddedLayout } from './embedded-body-layout';
+
+const embeddedRoot = document.getElementById( 'woocommerce-embedded-root' );
+
+if ( embeddedRoot ) {
+	const settingsGroup = 'wc_admin';
+	const hydrateUser = getAdminSetting( 'currentUserData' );
+
+	renderEmbeddedLayout( embeddedRoot, hydrateUser, settingsGroup );
+	renderCustomerEffortScoreTracks( embeddedRoot );
+}
