996b098e599ddd7f4ecd988e52b910d474914f69
Pavel Dohnal <pavel.dohnal@automattic.com>
2025-02-26 09:50:24 +0100

    Fix the email editor js code to match prettier config
    #55603

--- packages/js/email-editor/src/types/wordpress-modules.ts
+++ packages/js/email-editor/src/types/wordpress-modules.ts
@@ -153,7 +153,7 @@ declare module '@wordpress/core-data' {
 	): [
 		WPBlock[],
 		( blocks: BlockInstance[] ) => void,
-		( blocks: BlockInstance[] ) => void,
+		( blocks: BlockInstance[] ) => void
 	];
 	export type WPBlock = any;
 
