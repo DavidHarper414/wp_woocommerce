982cdc0f0153d62357f17f11fc073d53fd5ed6ad
paul sealock <psealock@gmail.com>
2025-02-26 17:25:38 +1300

    update tests

--- plugins/woocommerce/tests/php/includes/settings/class-wc-settings-emails-test.php
+++ plugins/woocommerce/tests/php/includes/settings/class-wc-settings-emails-test.php
@@ -84,8 +84,6 @@ class WC_Settings_Emails_Test extends WC_Settings_Unit_Test_Case {
 			'woocommerce_email_body_background_color'  => 'color',
 			'woocommerce_email_text_color'             => 'color',
 			'woocommerce_email_footer_text_color'      => 'color',
-			'email_merchant_notes'                     => array( 'title', 'sectionend' ),
-			'woocommerce_merchant_email_notifications' => 'checkbox',
 		);
 
 		$this->assertEquals( $expected, $setting_ids_and_types );
@@ -124,8 +122,6 @@ class WC_Settings_Emails_Test extends WC_Settings_Unit_Test_Case {
 			'email_color_palette'                      => 'email_color_palette',
 			'woocommerce_email_text_color'             => 'color',
 			'woocommerce_email_footer_text_color'      => 'color',
-			'email_merchant_notes'                     => array( 'title', 'sectionend' ),
-			'woocommerce_merchant_email_notifications' => 'checkbox',
 		);
 
 		$this->assertEquals( $expected, $setting_ids_and_types );
