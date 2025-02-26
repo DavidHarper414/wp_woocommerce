cea76b31ac84d0c2e55353d16f7ac1b7453a6b04
Tom Cafferkey <tjcafferkey@gmail.com>
2025-02-26 14:22:04 +0000

    Fix linter

--- plugins/woocommerce-blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image/frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image/frontend.tsx
@@ -1,11 +1,7 @@
 /**
  * External dependencies
  */
-import {
-	store,
-	getContext as getContextFn,
-	getElement,
-} from '@woocommerce/interactivity';
+import { store, getContext as getContextFn } from '@woocommerce/interactivity';
 import { StorePart } from '@woocommerce/utils';
 
 /**
