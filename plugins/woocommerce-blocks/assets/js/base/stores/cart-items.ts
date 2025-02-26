d8be9a3e6063f14f66cad169cff4eab16fb16c31
Sam Seay <samueljseay@gmail.com>
2025-02-26 13:55:31 +0800

    Fix formatting

--- plugins/woocommerce-blocks/assets/js/base/stores/cart-items.ts
+++ plugins/woocommerce-blocks/assets/js/base/stores/cart-items.ts
@@ -23,7 +23,7 @@ export type Store = {
 		};
 	};
 	actions: {
-		addCartItem: (args: { id: number; quantity: number }) => void;
+		addCartItem: ( args: { id: number; quantity: number } ) => void;
 		// Todo: Check why if I switch to an async function here the types of the store stop working.
 		refreshCartItems: () => void;
 	};
@@ -43,55 +43,56 @@ function isSuccessfulResponse(
 	res: Response,
 	json: CartItemsResponse | CartItemResponse
 ): json is Item | Item[] {
-	return res.status.toString().startsWith('2');
+	return res.status.toString().startsWith( '2' );
 }
 
-function generateError(json: StoreAPIError) {
-	return Object.assign(new Error(json.message || 'Unknown error.'), {
+function generateError( json: StoreAPIError ) {
+	return Object.assign( new Error( json.message || 'Unknown error.' ), {
 		code: json.code || 'unknown_error',
-	});
+	} );
 }
 
 let pendingRefresh = false;
 let refreshTimeout = 3000;
 
-function emitSyncEvent({
+function emitSyncEvent( {
 	quantityChanges,
 }: {
 	quantityChanges: QuantityChanges;
-}) {
+} ) {
 	window.dispatchEvent(
-		new CustomEvent('wc-blocks_store_sync_required', {
+		new CustomEvent( 'wc-blocks_store_sync_required', {
 			detail: {
 				type: 'from_iAPI',
 				quantityChanges,
 			},
-		})
+		} )
 	);
 }
 
-export const { state, actions } = store<Store>(
+export const { state, actions } = store< Store >(
 	'woocommerce',
 	{
 		actions: {
-			*addCartItem({ id, quantity }: { id: number; quantity: number }) {
+			*addCartItem( { id, quantity }: { id: number; quantity: number } ) {
 				let itemIndex = state.cart.items.findIndex(
-					({ id: productId }) => id === productId
+					( { id: productId } ) => id === productId
 				);
 				const previousQuantity =
-					state.cart.items[itemIndex]?.quantity ?? 0;
+					state.cart.items[ itemIndex ]?.quantity ?? 0;
 				let key: string | null = null;
 				const quantityChanges: QuantityChanges = {};
 
 				// Optimistically updates the number of items in the cart.
-				if (itemIndex !== -1) {
-					state.cart.items[itemIndex].quantity = quantity;
-					key = state.cart.items[itemIndex].key || null;
-					if (key) quantityChanges.cartItemsPendingQuantity = [key];
+				if ( itemIndex !== -1 ) {
+					state.cart.items[ itemIndex ].quantity = quantity;
+					key = state.cart.items[ itemIndex ].key || null;
+					if ( key )
+						quantityChanges.cartItemsPendingQuantity = [ key ];
 				} else {
-					state.cart.items.push({ id, quantity });
+					state.cart.items.push( { id, quantity } );
 					itemIndex = state.cart.items.length - 1;
-					quantityChanges.productsPendingAdd = [id];
+					quantityChanges.productsPendingAdd = [ id ];
 				}
 
 				// Updates the database.
@@ -101,36 +102,40 @@ export const { state, actions } = store<Store>(
 						// `/cart/update-item` because sometimes extenders can
 						// modify the quantities of other items on the server so we
 						// need to retrieve the whole cart each time.
-						`${state.restUrl}wc/store/v1/cart/items/${key || ''}`,
+						`${ state.restUrl }wc/store/v1/cart/items/${
+							key || ''
+						}`,
 						{
 							method: key ? 'PUT' : 'POST',
 							headers: {
 								Nonce: state.nonce,
 								'Content-Type': 'application/json',
 							},
-							body: JSON.stringify(state.cart.items[itemIndex]),
+							body: JSON.stringify(
+								state.cart.items[ itemIndex ]
+							),
 						}
 					);
 					const json: CartItemResponse = yield res.json();
 
 					// Checks if the response contains an error.
-					if (!isSuccessfulResponse(res, json))
-						throw generateError(json);
+					if ( ! isSuccessfulResponse( res, json ) )
+						throw generateError( json );
 
 					// Updates the local cart.
-					state.cart.items[itemIndex] = json;
+					state.cart.items[ itemIndex ] = json;
 
 					// dispatch legacy event
-					triggerAddedToCartEvent({
+					triggerAddedToCartEvent( {
 						preserveCartData: true,
-					});
+					} );
 
 					// Dispatches the event to sync the @wordpress/data store.
-					emitSyncEvent({ quantityChanges });
-				} catch (error) {
+					emitSyncEvent( { quantityChanges } );
+				} catch ( error ) {
 					// Reverts the optimistic update.
 					// Todo: Prevent racing conditions with multiple addToCart calls for the same item.
-					state.cart.items[itemIndex].quantity =
+					state.cart.items[ itemIndex ].quantity =
 						previousQuantity || 0;
 
 					throw error;
@@ -138,29 +143,29 @@ export const { state, actions } = store<Store>(
 			},
 			*refreshCartItems() {
 				// Skips if there's a pending request.
-				if (pendingRefresh) return;
+				if ( pendingRefresh ) return;
 
 				pendingRefresh = true;
 
 				try {
 					const res: Response = yield fetch(
-						`${state.restUrl}wc/store/v1/cart/items`,
+						`${ state.restUrl }wc/store/v1/cart/items`,
 						{ headers: { 'Content-Type': 'application/json' } }
 					);
 					const json: CartItemsResponse = yield res.json();
 
 					// Checks if the response contains an error.
-					if (!isSuccessfulResponse(res, json))
-						throw generateError(json);
+					if ( ! isSuccessfulResponse( res, json ) )
+						throw generateError( json );
 
 					// Updates the local cart.
 					state.cart.items = json;
 
 					// Resets the timeout.
 					refreshTimeout = 3000;
-				} catch (error) {
+				} catch ( error ) {
 					// Tries again after the timeout.
-					setTimeout(actions.refreshCartItems, refreshTimeout);
+					setTimeout( actions.refreshCartItems, refreshTimeout );
 
 					// Increases the timeout exponentially.
 					refreshTimeout *= 2;
@@ -175,12 +180,12 @@ export const { state, actions } = store<Store>(
 
 window.addEventListener(
 	'wc-blocks_store_sync_required',
-	async (event: Event) => {
-		const customEvent = event as CustomEvent<{
+	async ( event: Event ) => {
+		const customEvent = event as CustomEvent< {
 			type: string;
 			id: number;
-		}>;
-		if (customEvent.detail.type === 'from_@wordpress/data') {
+		} >;
+		if ( customEvent.detail.type === 'from_@wordpress/data' ) {
 			actions.refreshCartItems();
 		}
 	}
