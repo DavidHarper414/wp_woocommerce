5b0cd7228e6e159711cae6e89dab59ec005d0c90
Alba Rincón <alba.rincon@automattic.com>
2025-03-03 14:51:33 +0100

    Return always array from get_attributes

--- plugins/woocommerce/includes/wc-conditional-functions.php
+++ plugins/woocommerce/includes/wc-conditional-functions.php
@@ -334,10 +334,6 @@ if ( ! function_exists( 'meta_is_product_attribute' ) ) {
 		if ( $product && method_exists( $product, 'get_variation_attributes' ) ) {
 			$variation_attributes = $product->get_variation_attributes();
 			$attributes           = $product->get_attributes();
-			if ( is_null( $attributes ) ) {
-				return false;
-			}
-
 			return ( in_array( $name, array_keys( $attributes ), true ) && in_array( $value, $variation_attributes[ $attributes[ $name ]['name'] ], true ) );
 		} else {
 			return false;
