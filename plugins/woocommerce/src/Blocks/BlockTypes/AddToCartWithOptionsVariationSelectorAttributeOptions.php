5f6f612df1387139509386ee19546f962633df28
Maikel Perez <mdperez86@gmail.com>
2025-03-04 09:24:28 -0500

    Remove non needed imports in AddToCartWithOptionsVariationSelectorAttributeOptions

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptionsVariationSelectorAttributeOptions.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptionsVariationSelectorAttributeOptions.php
@@ -3,10 +3,6 @@ declare(strict_types=1);
 
 namespace Automattic\WooCommerce\Blocks\BlockTypes;
 
-use Automattic\WooCommerce\Admin\Features\Features;
-use Automattic\WooCommerce\Blocks\Utils\StyleAttributesUtils;
-use WP_Block;
-
 /**
  * Block type for variation selector attribute options in add to cart with options.
  * It's responsible to render the attribute options.
