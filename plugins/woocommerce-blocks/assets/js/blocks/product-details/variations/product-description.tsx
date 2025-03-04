bc13890bd48fbab28eadbde00f7743fee73c05eb
Manish Menaria <the.manish.menaria@gmail.com>
2025-03-04 14:36:09 +0530

    Update placeholder to make it specific to product description

--- plugins/woocommerce-blocks/assets/js/blocks/product-details/variations/product-description.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-details/variations/product-description.tsx
@@ -5,8 +5,10 @@ import { __ } from '@wordpress/i18n';
 import { Icon } from '@wordpress/components';
 import { page } from '@wordpress/icons';
 import { registerBlockVariation } from '@wordpress/blocks';
-import type { Block } from '@wordpress/blocks';
+import type { Block, BlockEditProps } from '@wordpress/blocks';
 import { addFilter } from '@wordpress/hooks';
+import { useBlockProps } from '@wordpress/block-editor';
+import clsx from 'clsx';
 
 export const CORE_NAME = 'core/post-content';
 export const VARIATION_NAME = 'woocommerce/product-description';
@@ -14,6 +16,11 @@ export const VARIATION_NAME = 'woocommerce/product-description';
 const BLOCK_TITLE = __( 'Product Description 1', 'woocommerce' );
 const BLOCK_DESCRIPTION = __(
 	'Displays the description of the product.',
+	'woocommerce'
+);
+
+const PLACEHOLDER_TEXT = __(
+	'This block displays the product description. When viewing a product page, the description content will automatically appear here.',
 	'woocommerce'
 );
 
@@ -66,5 +73,64 @@ const registerProductDescription = () => {
 		scope: [ 'block', 'inserter' ],
 	} );
 };
+
+interface ProductDescriptionAttributes {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	__woocommerceNamespace?: string;
+}
+
+interface ExtendedBlockEditProps
+	extends BlockEditProps< ProductDescriptionAttributes > {
+	name: string;
+	context?: {
+		postId?: number;
+		postType?: string;
+	};
+}
+
+const withProductDescriptionEdit =
+	( BlockEdit: React.ComponentType< ExtendedBlockEditProps > ) =>
+	( props: ExtendedBlockEditProps ) => {
+		const {
+			name,
+			context,
+			attributes,
+			__unstableLayoutClassNames: layoutClassNames,
+		} = props;
+
+		// If this is not our variation, return the original BlockEdit
+		if (
+			name !== CORE_NAME ||
+			attributes.__woocommerceNamespace !== VARIATION_NAME
+		) {
+			return <BlockEdit { ...props } />;
+		}
+
+		// If we have context, it means we're in a product context, so render the original BlockEdit of core/post-content
+		if ( context?.postId && context?.postType ) {
+			return <BlockEdit { ...props } />;
+		}
+
+		const blockProps = useBlockProps( {
+			className: clsx(
+				layoutClassNames,
+				'wc-block-product-description__placeholder'
+			),
+		} );
+
+		// Otherwise, render our custom placeholder
+		return (
+			<div { ...blockProps }>
+				<p>{ PLACEHOLDER_TEXT }</p>
+			</div>
+		);
+	};
+
+// Render Product Description placeholder if we're not in a product context
+addFilter(
+	'editor.BlockEdit',
+	'woocommerce/with-product-description-edit',
+	withProductDescriptionEdit
+);
 
 export default registerProductDescription;
