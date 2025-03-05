567a7c6a0311b0c9384f4d0ffffb03a7b97200b0
cpap <papazoglou.charalampos@gmail.com>
2025-03-05 18:07:15 +0200

    Product Gallery: shows all thumbnails in editor.

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/edit.tsx
@@ -5,6 +5,7 @@ import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { PanelBody } from '@wordpress/components';
 import { WC_BLOCKS_IMAGE_URL } from '@woocommerce/block-settings';
 import type { BlockEditProps } from '@wordpress/blocks';
+import { useEffect, useRef } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -16,7 +17,47 @@ export const Edit = ( {
 	attributes,
 	setAttributes,
 }: BlockEditProps< ProductGalleryThumbnailsBlockAttributes > ) => {
+	const blockRef = useRef< HTMLDivElement >( null );
+
+	useEffect( () => {
+		if ( ! blockRef.current ) return;
+
+		// The wrapper of the large image block might contain styles that affect the height (margin, padding, etc.).
+		const largeImageBlockWrapper =
+			blockRef.current.parentElement?.querySelector(
+				'[data-type="woocommerce/product-gallery-large-image"]'
+			)?.parentElement;
+
+		if ( ! largeImageBlockWrapper ) return;
+
+		const updateHeight = () => {
+			if ( ! blockRef.current ) return;
+			const computedStyle = window.getComputedStyle(
+				largeImageBlockWrapper
+			);
+			const marginTop = parseInt( computedStyle.marginTop, 10 );
+			const marginBottom = parseInt( computedStyle.marginBottom, 10 );
+			const totalHeight =
+				largeImageBlockWrapper.offsetHeight + marginTop + marginBottom;
+
+			blockRef.current.style.maxHeight = `${ totalHeight }px`;
+		};
+
+		// Initial height update
+		updateHeight();
+
+		// Create ResizeObserver to monitor the large image block
+		const resizeObserver = new ResizeObserver( updateHeight );
+		resizeObserver.observe( largeImageBlockWrapper );
+
+		// Cleanup
+		return () => {
+			resizeObserver.disconnect();
+		};
+	}, [ blockRef.current?.parentElement ] );
+
 	const blockProps = useBlockProps( {
+		ref: blockRef,
 		className: `wc-block-product-gallery-thumbnails`,
 	} );
 
