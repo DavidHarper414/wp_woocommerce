d43611a22e3d836ee5eaefa95476540a78d3a080
Lourens Schep <lourensschep@gmail.com>
2025-03-06 14:38:14 +0100

    Add example of review author variation block

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-reviews/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-reviews/index.tsx
@@ -11,6 +11,7 @@ import metadata from './block.json';
 import save from './save';
 import edit from './edit';
 import icon from './icon';
+import { registerProductReviewAuthorVariation } from './variations/review-author';
 
 if ( isExperimentalBlocksEnabled() ) {
 	// @ts-expect-error metadata is not typed.
@@ -19,4 +20,6 @@ if ( isExperimentalBlocksEnabled() ) {
 		edit,
 		save,
 	} );
+
+	registerProductReviewAuthorVariation();
 }
