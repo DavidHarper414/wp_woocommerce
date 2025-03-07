1763f0cddc1132cbac984f517fe2f8bfefaa921d
Manish Menaria <the.manish.menaria@gmail.com>
2025-03-07 16:07:43 +0530

    Update product description block to handle product-specific content

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-description/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-description/edit.tsx
@@ -88,6 +88,7 @@ function ReadOnlyContent( {
 }
 
 function EditableContent( { context = {} } ) {
+	// TODO At every place where postType is used, should I replace it with 'product'?
 	const { postType, postId } = context;
 
 	const [ blocks, onInput, onChange ] = useEntityBlockEditor(
@@ -112,6 +113,7 @@ function EditableContent( { context = {} } ) {
 	const initialInnerBlocks = [ [ 'core/paragraph' ] ];
 
 	const props = useInnerBlocksProps(
+		// TODO Change className?
 		useBlockProps( { className: 'entry-content' } ),
 		{
 			value: blocks,
@@ -126,11 +128,28 @@ function EditableContent( { context = {} } ) {
 function Content( props ) {
 	const { context: { queryId, postType, postId } = {}, layoutClassNames } =
 		props;
+	const blockProps = useBlockProps( { className: layoutClassNames } );
+
 	const userCanEdit = useCanEditEntity( 'postType', postType, postId );
 	if ( userCanEdit === undefined ) {
 		return null;
 	}
 
+	// TODO Review it again.
+	if ( postType && postType !== 'product' ) {
+		return (
+			<div { ...blockProps }>
+				<Warning>
+					{ __(
+						'This block can only be used with products.',
+						'woocommerce'
+					) }
+				</Warning>
+			</div>
+		);
+	}
+
+	// TODO Update this, as it's for Query Loop block.
 	const isDescendentOfQueryLoop = Number.isFinite( queryId );
 	const isEditable = userCanEdit && ! isDescendentOfQueryLoop;
 
@@ -152,20 +171,9 @@ function Placeholder( { layoutClassNames } ) {
 	return (
 		<div { ...blockProps }>
 			<p>
+				{ /* TODO Will there be always same placeholder text? Or should it change based on the context? */ }
 				{ __(
-					'This is the Content block, it will display all the blocks in any single post or page.',
-					'woocommerce'
-				) }
-			</p>
-			<p>
-				{ __(
-					'That might be a simple arrangement like consecutive paragraphs in a blog post, or a more elaborate composition that includes image galleries, videos, tables, columns, and any other block types.',
-					'woocommerce'
-				) }
-			</p>
-			<p>
-				{ __(
-					'If there are any Custom Post Types registered at your site, the Content block can display the contents of those entries as well.',
+					'This block displays the product description. When viewing a product page, the description content will automatically appear here.',
 					'woocommerce'
 				) }
 			</p>
@@ -187,14 +195,16 @@ function RecursionError() {
 	);
 }
 
-export default function PostContentEdit( {
+export default function ProductDescriptionEdit( {
 	context,
 	__unstableLayoutClassNames: layoutClassNames,
 	__unstableParentLayout: parentLayout,
 } ) {
+	// TODO Should I check if postType is product?
 	const { postId: contextPostId, postType: contextPostType } = context;
 	const hasAlreadyRendered = useHasRecursion( contextPostId );
 
+	// TODO Why there is recursion error when added to a new post?
 	if ( contextPostId && contextPostType && hasAlreadyRendered ) {
 		return <RecursionError />;
 	}
