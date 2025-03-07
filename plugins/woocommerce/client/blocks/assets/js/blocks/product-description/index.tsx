340c6bda5d710c3be2245ccceb4ba3f33a9a7b92
Manish Menaria <the.manish.menaria@gmail.com>
2025-03-07 16:08:37 +0530

    Remove console.log

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-description/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-description/index.tsx
@@ -11,9 +11,7 @@ import metadata from './block.json';
 import edit from './edit';
 import icon from './icon';
 
-console.log( 'isExperimentalBlocksEnabled', isExperimentalBlocksEnabled() );
 if ( isExperimentalBlocksEnabled() ) {
-	console.log( 'isExperimentalBlocksEnabled' );
 	// @ts-expect-error metadata is not typed.
 	registerBlockType( metadata, {
 		icon,
